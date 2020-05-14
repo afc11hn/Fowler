fee08258ecb4f413f1fb6b701e743ed166aa770a
Stefan Lukas <afc11hn@hotmail.de>
2020-05-14 14:39:24 +0200

    Extract rental calculation into rental class

--- src/excercise/Customer.java
+++ src/excercise/Customer.java
@@ -34,7 +34,7 @@ class Customer {
         StringBuilder result = new StringBuilder(format);
 
         for (Rental rental : rentals) {
-            double thisAmount = amountFor(rental);
+            double thisAmount = rental.amountFor();
             format = String.format("\t%s\t\t%d\t%s\n", rental.getMovie().getTitle(), rental.getDaysRented(), thisAmount);
             result.append(format);
             totalAmount += thisAmount;
@@ -45,25 +45,5 @@ class Customer {
         return result.toString();
     }
 
-    private double amountFor(final Rental rental) {
-        return switch (rental.getMovie().getType()) {
-            case REGULAR -> amountFor(rental, 2, 2, 1.5);
-            case NEW_RELEASE -> amountFor(rental, 0, 0, 3);
-            case CHILDRENS -> amountFor(rental, 1.5, 3, 1.5);
-        };
-    }
-
-    private static double amountFor(final Rental rental, final double basePrice, final int maximumRentalDays, final double rentalRate) {
-        final int daysRented = rental.getDaysRented();
-
-        if (daysRented>maximumRentalDays) {
-            final int rentalDuration = daysRented - maximumRentalDays;
-            final double rentalPrice = (double) rentalDuration * rentalRate;
-
-            return basePrice + rentalPrice;
-        }
-
-        return basePrice;
-    }
 }
     
--- src/excercise/Rental.java
+++ src/excercise/Rental.java
@@ -9,11 +9,33 @@ class Rental {
         this.daysRented = daysRented;
     }
 
+    static double amountFor(final Rental rental, final double basePrice, final int maximumRentalDays, final double rentalRate) {
+        final int daysRented = rental.getDaysRented();
+
+        if (daysRented>maximumRentalDays) {
+            final int rentalDuration = daysRented - maximumRentalDays;
+            final double rentalPrice = (double) rentalDuration * rentalRate;
+
+            return basePrice + rentalPrice;
+        }
+
+        return basePrice;
+    }
+
     public int getDaysRented() {
         return daysRented;
     }
 
     public Movie getMovie() {
         return movie;
+    }
+
+
+    double amountFor() {
+        return switch (getMovie().getType()) {
+            case REGULAR -> amountFor(this, 2, 2, 1.5);
+            case NEW_RELEASE -> amountFor(this, 0, 0, 3);
+            case CHILDRENS -> amountFor(this, 1.5, 3, 1.5);
+        };
     }
 }
