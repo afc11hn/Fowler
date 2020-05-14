7da4b431698c412a8df4d24bd1e06341d0e8477b
Stefan Lukas <afc11hn@hotmail.de>
2020-05-14 14:32:53 +0200

    Extract rental calculation into separate method

--- src/excercise/Customer.java
+++ src/excercise/Customer.java
@@ -33,7 +33,7 @@ class Customer {
         String format = String.format("excercise.Rental Record for %s\n\tTitle\t\tDays\tAmount\n", this.getName());
         StringBuilder result = new StringBuilder(format);
 
-        for (Rental rental: rentals) {
+        for (Rental rental : rentals) {
             double thisAmount = amountFor(rental);
             format = String.format("\t%s\t\t%d\t%s\n", rental.getMovie().getTitle(), rental.getDaysRented(), thisAmount);
             result.append(format);
@@ -46,26 +46,24 @@ class Customer {
     }
 
     private double amountFor(final Rental rental) {
-        double thisAmount = 0;
-        switch (rental.getMovie().getType()) {
-            case REGULAR:
-                thisAmount += 2;
-                if (rental.getDaysRented() > 2) {
-                    thisAmount += (rental.getDaysRented() - 2) * 1.5;
-                }
-                break;
-            case NEW_RELEASE:
-                thisAmount += rental.getDaysRented() * 3;
-                break;
-            case CHILDRENS:
-                thisAmount += 1.5;
-                if (rental.getDaysRented() > 3) {
-                    thisAmount += (rental.getDaysRented() - 3) * 1.5;
-                }
-                break;
-        }
-        return thisAmount;
+        return switch (rental.getMovie().getType()) {
+            case REGULAR -> amountFor(rental, 2, 2, 1.5);
+            case NEW_RELEASE -> amountFor(rental, 0, 0, 3);
+            case CHILDRENS -> amountFor(rental, 1.5, 3, 1.5);
+        };
     }
 
+    private static double amountFor(final Rental rental, final double basePrice, final int maximumRentalDays, final double rentalRate) {
+        final int daysRented = rental.getDaysRented();
+
+        if (daysRented>maximumRentalDays) {
+            final int rentalDuration = daysRented - maximumRentalDays;
+            final double rentalPrice = (double) rentalDuration * rentalRate;
+
+            return basePrice + rentalPrice;
+        }
+
+        return basePrice;
+    }
 }
     
